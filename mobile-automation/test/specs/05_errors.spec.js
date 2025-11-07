eecda592183ea52ef6da0b7e1a4a4dde49addca0
Gustavo Benevides <gustavobsimoes1991@gmail.com>
2025-11-07 08:47:12 -0300

    Update 05_errors.spec.js

--- mobile-automation/test/specs/05_errors.spec.js
+++ mobile-automation/test/specs/05_errors.spec.js
@@ -1,5 +1,5 @@
-const HomePage = import('../../src/pageobjects/home.page')
-const FormsPage = import('../../src/pageobjects/forms.page')
+const HomePage = import('../../src/pageobjects/home.page.js')
+const FormsPage = import('../../src/pageobjects/forms.page.js')
 describe('Mensagens de erro e validação', () => {
   it('Deve mostrar mensagem de validação quando campo obrigatório vazio', async () => {
     await HomePage.openForms()
