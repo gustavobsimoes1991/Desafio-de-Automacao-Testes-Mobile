8a9e1984c25233360bc40e51120368c07ee28a9c
Gustavo Benevides <gustavobsimoes1991@gmail.com>
2025-11-07 08:41:20 -0300

    Update 04_forms.spec.js

--- mobile-automation/test/specs/04_forms.spec.js
+++ mobile-automation/test/specs/04_forms.spec.js
@@ -1,5 +1,5 @@
-const HomePage = require('../../src/pageobjects/home.page')
-const FormsPage = require('../../src/pageobjects/forms.page')
+const HomePage = import('../../src/pageobjects/home.page')
+const FormsPage = import('../../src/pageobjects/forms.page')
 describe('Forms', () => {
   it('Deve preencher e submeter formulário com sucesso', async () => {
     await HomePage.openForms()
