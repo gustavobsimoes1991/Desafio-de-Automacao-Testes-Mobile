597b1ee4ad2f419afca6519686652be0de5a5ced
Gustavo Benevides <gustavobsimoes1991@gmail.com>
2025-11-07 08:42:07 -0300

    Update 08_screenshots.spec.js

--- mobile-automation/test/specs/08_screenshots.spec.js
+++ mobile-automation/test/specs/08_screenshots.spec.js
@@ -1,5 +1,5 @@
-const HomePage = require('../../src/pageobjects/home.page')
-const { save } = require('../../src/utils/screenshotHelper')
+const HomePage = import('../../src/pageobjects/home.page')
+const { save } = import('../../src/utils/screenshotHelper')
 describe('Evidencias - Screenshots', () => {
   it('Deve capturar screenshot da home', async () => {
     const filename = await save('home-screen')
