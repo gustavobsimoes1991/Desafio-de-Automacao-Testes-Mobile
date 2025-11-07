b79cf496bfb2cd8e7aae9e0d25d02ac7dbed8190
Gustavo Benevides <gustavobsimoes1991@gmail.com>
2025-11-07 07:57:16 -0300

    Create 10_logout.spec.js

---
+++ mobile-automation/test/specs/10_logout.spec.js
@@ -0,0 +1,7 @@
+const HomePage = require('../../src/pageobjects/home.page')
+describe('Logout', () => {
+  it('Deve efetuar logout com sucesso', async () => {
+    await HomePage.logout()
+    expect(await $('~login-title').isDisplayed()).to.be.true
+  })
+})
