ae6d77861e87cb1ac82d44fc59def6999e28c1c4
Gustavo Benevides <gustavobsimoes1991@gmail.com>
2025-11-07 08:51:24 -0300

    Update wdio.conf.js

--- mobile-automation/wdio.conf.js
+++ mobile-automation/wdio.conf.js
@@ -1,22 +1,44 @@
 import AppiumService from '@wdio/appium-service'
 
 export const config = {
-
+  runner: 'local',
   port: 4723,
-  // ...
+  specs: [
+    './test/specs/**/*.js',
+    './test/specs/**/*.ts'
+  ],
+  maxInstances: 1,
+  capabilities: [{
+    platformName: 'Android',
+    'appium:deviceName': 'Android Emulator',
+    'appium:platformVersion': '13.0',
+    'appium:automationName': 'UiAutomator2',
+    'appium:app': './apps/native-demo-app.apk',
+    'appium:autoGrantPermissions': true
+  }],
   services: [
     [AppiumService, {
-   
       appiumArgs: ['--base-path', '/wd/hub'],
       command: 'appium',
       logPath: './appium-logs',
       args: {
         basePath: '/wd/hub',
         port: 4723,
-  
-        timeout: 60000 
+        timeout: 60000
       }
     }]
   ],
-
+  framework: 'mocha',
+  reporters: ['spec', ['allure', { outputDir: 'allure-results' }]],
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 60000
+  },
+  autoCompileOpts: {
+    autoCompile: true,
+    tsNodeOpts: {
+      transpileOnly: true,
+      project: './tsconfig.json',
+    }
+  }
 }
