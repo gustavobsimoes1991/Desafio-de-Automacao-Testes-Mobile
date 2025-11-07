17bb0b1e4930794865d9ddb823cfdfb2d38e885e
Gustavo Benevides <gustavobsimoes1991@gmail.com>
2025-11-07 07:56:16 -0300

    Create 06_settings.spec.js

---
+++ mobile-automation/test/specs/06_settings.spec.js
@@ -0,0 +1,11 @@
+const HomePage = require('../../src/pageobjects/home.page')
+const SettingsPage = require('../../src/pageobjects/settings.page')
+describe('Settings', () => {
+  it('Deve alterar notificação e salvar perfil', async () => {
+    await HomePage.openSettings()
+    await SettingsPage.setNotification(true)
+    await SettingsPage.saveProfile('Profile atualizado')
+    const toast = await $('~toast').getText()
+    expect(toast.toLowerCase()).to.include('saved')
+  })
+})
